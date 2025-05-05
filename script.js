25027301d5a1879cfc35fb072e48fa277c939c9e
aiplannerasteron <foxya404@gmail.com>
2025-05-05 22:44:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Локализация
+    // Локализация Стрендж Юниверса
     const translations = {
         ru: {
             title: "ИИ-Планировщик задач",
